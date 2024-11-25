7db89712059e7a80d45f46068969c75ff421f07b
xmiaocat <xincheng.miao@stud-mail.uni-wuerzburg.de>
2024-11-25 18:35:32 +0100

    Fix overflow error for heavier atoms

--- src/dftd3/reference3.rs
+++ src/dftd3/reference3.rs
@@ -3,14 +3,16 @@ use crate::dftd3::reference3_inc::{C6AB};
 pub const MREF: usize = 5;
 
 pub fn get_c6(iref: usize, jref: usize, ati: u8, atj: u8) -> f64 {
-    let (ic, c6): (u8, f64);
+    let (ic, c6): (usize, f64);
+    let ati = ati as usize;
+    let atj = atj as usize;
 
     if ati > atj {
         ic = atj + ati * (ati - 1)/2 - 1;
-        c6 = C6AB[ic as usize][jref][iref];
+        c6 = C6AB[ic][jref][iref];
     } else {
         ic = ati + atj * (atj - 1)/2 - 1;
-        c6 = C6AB[ic as usize][iref][jref];
+        c6 = C6AB[ic][iref][jref];
     };
 
    c6
